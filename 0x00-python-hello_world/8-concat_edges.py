207621cfd32f4e511f5c64ff49454d54711eb9e7
Ailwei <Ailweimaemu@gmail.com>
2023-10-02 15:06:09 -0700

    8. Create a new sentence

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming"
+str += "\nlanguage that combines remarkable power with very clear syntax"
+print(str)
+
