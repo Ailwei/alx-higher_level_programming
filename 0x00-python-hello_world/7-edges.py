e0b3e20d0169abdeab87ba480076a7a61303ff72
Ailwei <Ailweimaemu@gmail.com>
2023-10-02 15:01:48 -0700

    7. Copy - Cut - Paste

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3, middle_word, word_last_2 = word[:3], word[1:-1], word[-2:]
-print(f"word_first_3: {word_first_3}")
-print(f"middle_word: {middle_word}")
-print(f"word_last_2: {word_last_2}")
+word_first_3 = word[0:3]
+middle_word = word[1:-1]
+word_last_2 = word[-2:]
+print("word_first_3: {}".format(word_first_3))
+print("middle_word: {}".format(middle_word))
+print("word_last_2: {}".format(word_last_2))
