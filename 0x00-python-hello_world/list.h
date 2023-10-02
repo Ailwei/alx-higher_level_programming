5f911e6c13d5250c129c092ce65f938712e2c104
Ailwei <Ailweimaemu@gmail.com>
2023-10-02 12:36:20 -0700

    updated list.h file

--- 0x00-python-hello_world/list.h
+++ 0x00-python-hello_world/list.h
@@ -1,7 +1,18 @@
 #ifndef LIST_H
 #define LIST_H
 
+/* Define a structure for a singly linked list node */
+typedef struct ListNode {
+    int data;       
+    struct ListNode* next; 
+} ListNode;
 
+/* Function prototypes */
+ListNode* createNode(int data);
+void insertNode(ListNode** head, int data);
+void deleteNode(ListNode** head, int data);
+void printList(ListNode* head);
+void freeList(ListNode* head);
 
 
 
