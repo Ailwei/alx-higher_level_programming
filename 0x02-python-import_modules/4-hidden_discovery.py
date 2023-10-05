285235420db4c5b10dca9682f51576cdaaa2657a
Ailwei <Ailweimaemu@gmail.com>
2023-10-05 08:23:23 -0700

    4. Who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import marshal
+import sys
+
+def extract_names(code_object):
+    # Extract the names defined in the code object
+    names = [name for name in code_object.co_names if not name.startswith('__')]
+    return names
+
+if __name__ == "__main__":
+    try:
+        pyc_filename = 'hidden_4.pyc'
+
+        # Read the compiled code from the .pyc file
+        with open(pyc_filename, 'rb') as pyc_file:
+            pyc_data = pyc_file.read()
+
+        # Unmarshal the compiled code to get the code object
+        code_object = marshal.loads(pyc_data[16:])
+
+        # Extract and print the names defined in the module
+        names = extract_names(code_object)
+        for name in sorted(names):
+            print(name)
+
+    except Exception as e:
+        print("Error:", e)
