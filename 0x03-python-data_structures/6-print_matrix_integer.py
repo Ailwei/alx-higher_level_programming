a3ffb636fa3c0462b4be8606d3466e4c5b4254ce
Ailwei <Ailweimaemu@gmail.com>
2023-10-09 16:06:07 -0700

    6. Lists of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        print("--")
+        print()
         return
 
-    max_width = len(str(max(max(matrix, key=lambda x: max(x)))))
-
     for row in matrix:
-        if not row:
-            print("$")
-        else:
-            for i, num in enumerate(row):
-                if i < len(row) - 1:
-                    print("{:d} ".format(num), end="")
-                else:
-                    print("{:d}${}".format(num), end="\n" if row == matrix[-1] else "")
-    print("--")
+        i = 0
+        while i < len(row):
+            print("{:d}".format(row[i]), end="")
+            if i < len(row) - 1:
+                print(" ", end="")
+            i += 1
+        print("$")
