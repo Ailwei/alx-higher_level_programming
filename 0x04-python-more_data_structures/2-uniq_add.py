bfee3d3e35064853996a713c7868ad97c067c6b5
Ailwei <61516275+Ailwei@users.noreply.github.com>
2023-10-11 16:14:31 +0200

    2. Unique addition

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,9 +3,9 @@
 def uniq_add(my_list=[]):
     unique_numbers = set()
     total = 0
-    
+
     for i in my_list:
         if i not in unique_numbers:
-           unique_numbers.add(i)
-           total += i
-    return total        
+            unique_numbers.add(i)
+            total += i
+    return total 
