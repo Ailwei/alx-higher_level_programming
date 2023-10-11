4a92c1c36ff5aeb84f760317e9ee88731a0cceac
Ailwei <Ailweimaemu@gmail.com>
2023-10-11 15:40:19 +0000

    4. Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,5 +6,8 @@ def only_diff_elements(set_1, set_2):
     for x in set_1:
         if x not in set_2:
             new_set.add(x)
+    for x in set_2:
+        if x not in set_1:
+             new_set.add(x)
 
     return new_set
