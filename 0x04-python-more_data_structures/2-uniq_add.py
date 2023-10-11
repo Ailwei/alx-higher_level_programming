06cc062f2f6eb2b764712374817231551b99213c
Ailwei <Ailweimaemu@gmail.com>
2023-10-11 13:51:14 +0000

    2. Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    unique_numbers = set()
+    total = 0
+    
+    for i in my_list:
+        if i not in unique_numbers:
+           unique_numbers.add(i)
+           total += i
+    return total        
