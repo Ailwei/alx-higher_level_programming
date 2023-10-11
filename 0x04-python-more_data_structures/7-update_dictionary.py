dd61057bde9aebec54540dfc000686b4825ed21f
Ailwei <Ailweimaemu@gmail.com>
2023-10-11 21:14:10 +0000

    7. Update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    found = False
-    for existing_key in a_dictionary:
-        if existing_key == key:
-            a_dictionary[existing_key] = value
-            found = True
-            break
-
-    if not found:
-        a_dictionary[key] = value
+    a_dictionary[key] = value
+    return a_dictionary
