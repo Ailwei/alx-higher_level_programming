87b8284ed573c621137cb23e6fbd35a5eed01bbd
Ailwei <61516275+Ailwei@users.noreply.github.com>
2023-10-11 21:48:05 +0200

    6. Print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+
+    for key in new_keys:
+        value = a_directory[keys]
+        print(f"{key}: {value}")
