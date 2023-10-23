f8653e9d4a0be367ab515849fa7f5958fd8200c6
Ailwei <Ailweimaemu@gmail.com>
2023-10-23 03:14:11 -0700

    push to git

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0  # To keep track of the number of integers printed
+    
+    try:
+        for item in my_list:
+            if isinstance(item, int):
+                print("{:d}".format(item), end=" ")
+                count += 1
+
+            if count == x:
+                break  # Stop when x integers have been printed
+    
+        print()
+        return count
+    except TypeError:
+        return count
---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None  # Set result to None if division by zero occurs
+    except Exception:
+        result = None  # Set result to None for other exceptions
+    print("Inside result: {:.1f}".format(result))  # Print the result
+    return result
+
