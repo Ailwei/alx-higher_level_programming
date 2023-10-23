4483cc92fbe8eeab070096d62af720805f079b83
Ailwei <Ailweimaemu@gmail.com>
2023-10-23 01:18:22 -0700

    0. Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+def safe_print_list(my_list=[], x=0):
+    count = 0;
+    try:
+        for i in range(x):
+            item = my_list[i]
+            print(item, end=" ")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
+
