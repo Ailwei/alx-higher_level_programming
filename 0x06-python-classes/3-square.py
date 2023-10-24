b8e703125e8f9b17b4b3dca20c96d855b3e3e7e4
Ailwei <Ailweimaemu@gmail.com>
2023-10-24 04:07:29 -0700

    3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 
+"""A module that contain square class"""
+
+
 class Square:
+    """Defines a 2D square with four equal sides and right angles.
+
+Args:
+    size (int): The size parameter that initializes
+    the private instance attribute __size.
+
+Attributes:
+    __size (int): A private integer instance attribute.
+
+Raises:
+    TypeError: If __size attribute is not of integer type.
+    ValueError: If __size attribute is less than zero.
+"""
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an Integer")
