48c15e3420300e2de167b864de69d264e89e05fd
Ailwei <Ailweimaemu@gmail.com>
2023-10-24 04:01:07 -0700

    2. Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 
+"""A module that contain Square class
+"""
+
+
 class Square:
+
+    """Represents a 2D polygon with 4 sizes and perpendicular sides
+
+    Args:
+         size (int): a parameter that intializes ``__size``
+         private instance attribute
+
+    Attributes:
+         __size (int): a private intstance attribute.
+
+    Raises:
+         TypeError: if ``__size`` attribute is not integer type.
+         ValueError: if ``__size`` attribute is less than zero.
+    """
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
