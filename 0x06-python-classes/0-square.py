4a71d1e74d8a739618aeb46061cce3fd2ce95388
Ailwei <Ailweimaemu@gmail.com>
2023-10-24 03:35:22 -0700

    0. My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    This is the Square class.
+
+    It doesn't have any attributes or methods yet.
+
+    You can use this class as a template to
+    define squares with specific behaviors.
+    """
     pass
