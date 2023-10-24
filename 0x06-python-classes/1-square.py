01d044f3413e132c3c43a030b7c33b03a084c740
Ailwei <Ailweimaemu@gmail.com>
2023-10-24 03:56:03 -0700

    1. Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+"""this contains square class"""
+
 
 class Square:
+    """"a class square that defines a square with private instance attracribute
+    args:
+    value (int): a value that initilises ''_size'' attributr
+    attributes:
+    _size (int):  a private instance attribute
+                       The size of a square is crucial for a square,
+                       many things depend of it (area computation, etc.),
+                       so i, as class builder, must control the type and
+                       value of this attribute.
+                       One way to have the control is to keep it privately.
+    """
     def __init__(self, size):
         self.__size = size
