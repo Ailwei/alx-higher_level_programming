eafb330cef53fd78d384a7fa45930a75c580fb84
Ailwei <Ailweimaemu@gmail.com>
2023-10-31 02:27:13 -0700

    3. String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-"""this defines the class rectangle"""
+"""A class that represents a rectangle"""
 
 
 class Rectangle:
+    """A blueprint for a rectangle"""
+
     def __init__(self, width=0, height=0):
-        """Initialize the Rectangle.
+        """Create an instance of the Rectangle class.
 
         Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
+            initial_width (int): The initial width of the rectangle.
+            initial_height (int): The initial height of the rectangle.
+
+        Raises:
+            TypeError: If width or height are not integers.
+            ValueError: If width or height are less than zero.
         """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
@@ -19,21 +25,21 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, value):
+    def width(self, width):
         """Set the width of the rectangle.
 
         Args:
-            value (int): The width of the rectangle.
+            new_width (int): The new width of the rectangle.
 
         Raises:
-            TypeError: If width is not an integer.
-            ValueError: If width is less than zero.
+            TypeError: If new_width is not an integer.
+            ValueError: If new_width is less than zero.
         """
-        if not isinstance(value, int):
+        if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if width < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        self.__width = width
 
     @property
     def height(self):
@@ -41,42 +47,39 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(self, value):
+    def height(self, height):
         """Set the height of the rectangle.
 
         Args:
-            value (int): The height of the rectangle.
+            new_height (int): The new height of the rectangle.
 
         Raises:
-            TypeError: If height is not an integer.
-            ValueError: If height is less than zero.
+            TypeError: If new_height is not an integer.
+            ValueError: If new_height is less than zero.
         """
-        if not isinstance(value, int):
+        if not isinstance(height, int):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self.__height = height
 
     def area(self):
-        """Calculate the area of the rectangle."""
+        """Calculate and return the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Calculate the perimeter of the rectangle.
-
-        Returns:
-            The perimeter of the rectangle.
-        """
+        """Calculate and return the perimeter of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
 
-    def __str__(self):
-        """Return a string representation of the rectangle.
-
-        Returns:
-            A string with '#' characters representing the rectangle.
-        """
+    def __str__(self) -> str:
+        """Return a string representation of the rectangle as a diagram."""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join(['#' * self.__width] * self.__height)
+        rectangle_str = ""
+        for _ in range(self.__height):
+            rectangle_str += "#" * self.__width
+            if _ < self.__height - 1:
+                rectangle_str += "\n"
+        return rectangle_str
