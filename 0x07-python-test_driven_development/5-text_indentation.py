289d1245bb38ddf067ea609230c654f3606abf22
Ailwei <Ailweimaemu@gmail.com>
2023-10-31 14:13:00 -0700

    5. Max integer - Unittest

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
 
+"""
+This module contains a function that indents text.
+"""
+
 def text_indentation(text):
+    """
+    This function prints text with 2 new lines after each ".", "?", or ":"
+
+    Args:
+        text (str): The string to be formatted
+
+    Raises:
+        TypeError: If text is not a string
+    """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    result = ""
-    for char in text:
-        result += char
-        if char in ".?:":
-            result += "\n\n"
-    lines = result.split("\n")
-    cleaned_lines = [line.strip() for line in lines]
-    formatted_text = "\n".join(cleaned_lines)
-    return formatted_text
+    cnt = 0
+    while cnt < len(text) and text[cnt] == " ":
+        cnt += 1
+
+    while cnt < len(text):
+        print(text[cnt], end="")
+        if text[cnt] == "\n" or text[cnt] in ".?:":
+            if text[cnt] in ".?:":
+                print("\n")
+            cnt += 1
+            while cnt < len(text) and text[cnt] == " ":
+                cnt += 1
+            continue
+        cnt += 1
