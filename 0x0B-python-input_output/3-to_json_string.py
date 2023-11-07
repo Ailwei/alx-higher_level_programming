47634a60ec000cd8c0ef89d731d779ab01e0328a
Ailwei <Ailweimaemu@gmail.com>
2023-11-07 05:32:29 -0800

    3. To JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Return the JSON representation of an object as a string.
+
+    :param my_obj: The object to be converted to JSON.
+    :return: A JSON string representation of the object.
+    :rtype: str
+    """
+    return json.dumps(my_obj)
