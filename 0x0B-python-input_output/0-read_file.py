1162625329a212b92dc38d5f4acb60e9145e3326
Ailwei <Ailweimaemu@gmail.com>
2023-11-07 03:35:34 -0800

    push read me file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,14 +5,14 @@ this define the read_file function
 
 
 def read_file(filename=""):
-      """
+    """
     Read and print the content of a text file in UTF-8 encoding.
 
     :param filename: The path to the text file to be read.
     :type filename: str
     """
     try:
-        with open(filename,mode='r',encoding='utf-8') as file:
+        with open(filename, mode='r', encoding='utf-8') as file:
             for line in file:
                 print(line, end="")
     except FileNotFoundError:
