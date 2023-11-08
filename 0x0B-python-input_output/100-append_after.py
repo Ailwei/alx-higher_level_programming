f54a0c23e3042f316b9909314fbe7fdf8e27d29c
Ailwei <Ailweimaemu@gmail.com>
2023-11-08 03:54:21 -0800

    13. Search and update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,21 +6,18 @@ append function definition
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    Inserts a line of text after each line containing a specific string in a file.
+    Inserts text after each line containing a given string in a file.
 
     Args:
-        filename (str): The name of the file to process.
-        search_string (str): The string to search for in each line.
-        new_string (str): The line of text to insert after lines containing the search string.
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
     """
-    if filename and search_string and new_string:
-        try:
-            with open(filename, "r") as file:
-                lines = file.readlines()
-                with open(filename, "w") as file:
-                    for line in lines:
-                        file.write(line)
-                        if search_string in line:
-                            file.write(new_string + '\n')
-        except:
-            pass
+    text = ""
+    with open(filename) as faila:
+        for line in faila:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w") as W:
+        W.write(text)
