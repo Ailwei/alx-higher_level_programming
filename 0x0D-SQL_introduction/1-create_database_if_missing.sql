c6437a7ed4f1570dfb74a0c2efa61c48f5e9d2c0
Ailwei <Ailweimaemu@gmail.com>
2023-12-12 01:17:05 -0800

    1. Create a database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,9 @@
+-- script that create database hbtn_0_0 in your MYSQL server
+-- if the database hbtn_0c_0 already exists. your script should not fail.
+-- you are not allowed to use Select OR show STATEMENTS.
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- switch to the hbtn_0c_0 databse
+
+USE hbtn_0c_0;
