ca1cd7b98c4a2dd0c12d2e96423e490e920b9c08
Ailwei <Ailweimaemu@gmail.com>
2023-12-12 04:31:24 -0800

    9. Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,15 @@
 -- the script creates second_table in the current database and add two multiple rows.
- create table if not exists SECOND_TABLE (
-	id INT,
-	name VARCHAR(256),
-	score INT
-	);
-INSERT INTO second_table (id, name, score) VALUES
-(1, 'John', 10);
-(2, 'Alex', 3);
-(3, 'Bob', 14);
-(4, 'George', 8);
+
+-- Creates and fills a table second_table with attributes id, name, and score
+CREATE TABLE IF NOT EXISTS `second_table` (
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
+
+-- Insert rows into second_table
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
 
