689b1ed413a317d09f3bbfb50e3ed46be174e182
Ailwei <Ailweimaemu@gmail.com>
2023-12-13 03:28:15 -0800

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,14 @@
--- script that creates the database hbtn_0d_2 and the user user_0d_2
+-- MySQL script to create hbtn_0d_2 database and user_0d_2
 
+-- Create the database hbtn_0d_2 if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER NOT EXISTS user_0D_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
+-- Create user_0d_2 if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant SELECT privilege on all tables in hbtn_0d_2 to user_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
+
