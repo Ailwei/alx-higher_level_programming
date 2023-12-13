f8ee0ce3421ac207fcf55b62effaafc24efb7ede
Ailwei <Ailweimaemu@gmail.com>
2023-12-13 06:24:40 -0800

    20. Best genre

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,18 @@
+-- script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+
+SELECT
+    tv_genres.name,
+    SUM(tv_show_genres_ratings.rating) AS rating_sum
+FROM
+    tv_genres
+JOIN
+    tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN
+    tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN
+    tv_show_genres_ratings ON tv_show_genres.id = tv_show_genres_ratings.show_genre_id
+GROUP BY
+    tv_genres.id, tv_genres.name
+ORDER BY
+    rating_sum DESC;
+
