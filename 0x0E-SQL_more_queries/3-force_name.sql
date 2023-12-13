450beeb0c1bf89f72597dda74198afeb20dfbe23
Ailwei <Ailweimaemu@gmail.com>
2023-12-13 03:46:55 -0800

    3. Always a name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,7 @@
 -- Script that creat the table force om your MYSQL server
 
 USE hbtn_0d_2;
-CREATE TABLE IF NOT EXISTS force_name (id INT, name (VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS force_name (
+	id INT,
+	name (VARCHAR(256) NOT NULL
+	);
