a5694ff69ece2c2bca049775d4575cc2a389708c
Ailwei <Ailweimaemu@gmail.com>
2023-12-13 06:04:11 -0800

    16. List shows and genres

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,4 @@
+-- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+
+SELECT tv_shows.title AS show_title, IFNULL(tv_genres.name, 'NULL') AS genre_name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id ORDER BY show_title ASC, genre_name ASC;
+
