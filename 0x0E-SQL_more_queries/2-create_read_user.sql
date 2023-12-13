087bce61d0981d8b2dda991f3a37007e1218c8ed
Ailwei <Ailweimaemu@gmail.com>
2023-12-13 03:26:39 -0800

    2. Read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,7 +2,5 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER NOT EXISTS user_0d_2.* TO 'user_0d_2'@'%';
-
-FLUSH PRIVILEDGES;
-
+CREATE USER NOT EXISTS user_0D_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
