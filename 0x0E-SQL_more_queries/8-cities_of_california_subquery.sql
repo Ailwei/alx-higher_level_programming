2172fa6ec20893fd8b64a29682dfc4ef9983008b
Ailwei <Ailweimaemu@gmail.com>
2023-12-13 04:57:20 -0800

    8. Cities of California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,11 +1,3 @@
--- MySQL script to list cities in California without using JOIN
-
-USE hbtn_0d_usa; -- Replace with your actual database name
-
--- Query to list cities in California
-SELECT id, name
-FROM cities
-WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id ASC;
-
+-- MySQL query to list all the cities of California in the hbtn_0d_usa database
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
 
