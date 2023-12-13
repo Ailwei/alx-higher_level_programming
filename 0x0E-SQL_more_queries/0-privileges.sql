8ee5cec9dbc6c48a0c8c9507063b993b375db9d2
Ailwei <Ailweimaemu@gmail.com>
2023-12-13 02:54:50 -0800

    0. My privileges!

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- MySQL script to show privileges for users
+
+-- Display privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Display privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
