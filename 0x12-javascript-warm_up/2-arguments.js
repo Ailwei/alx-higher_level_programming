84ac9247c1b3f89f03ec012c5e811d4e88eb7f36
Ailwei <Ailweimaemu@gmail.com>
2024-01-08 03:05:33 -0800

    2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const number_args = process.argv.length - 2;
+
+if (number_args == 0)
+{
+	console.log('NO argument');
+}
+else if (number_args === 1)
+{
+	console.log('Argument found');
+}
+else 
+{
+	console.log('Argument found');
+}
