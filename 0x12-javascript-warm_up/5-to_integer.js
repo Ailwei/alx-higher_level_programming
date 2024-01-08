ba680a331e301307046bdbadc2b93a39f494f813
Ailwei <Ailweimaemu@gmail.com>
2024-01-08 04:42:27 -0800

    5. An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,11 +2,8 @@
 
 const mynumb = parseInt(process.argv[2]);
 
-if (!isNaN(mynumb))
-{
-	console.log(`My number: ${mynumb}`);
-}
-else
-{
-	console.log('Not a number');
+if (!isNaN(mynumb)) {
+  console.log(`My number: ${mynumb}`);
+} else {
+  console.log('Not a number');
 }
