bd2408eda19782a7742db729842314a5b33935fb
Ailwei <Ailweimaemu@gmail.com>
2024-01-09 09:12:41 -0800

    6. Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,22 +2,16 @@
 
 const SquareBase = require('./5-square');
 
-
-class Square extends SquareBase
-{
-	charPrint(c)
-	{
-		if (c === undefined)
-		{
-			c = 'X';
-		}
-		if (this.width !== undefined && this.height !== undefined)
-		{
-			for (let i = 0; i < this.height; i++)
-			{
-				console.log(c.repeat(this.width));
-			}
-		}
-	}
+class Square extends SquareBase {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
 }
 module.exports = Square;
