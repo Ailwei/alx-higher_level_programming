f37ab35776dde29a8e9903bd004f1d0cd326c06c
Ailwei <Ailweimaemu@gmail.com>
2024-01-09 11:55:07 -0800

    12. Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 
-
 const dict = require('./101-data');
 
-
-const occurrencesDict = {}
+const occurrencesDict = {};
 for (const userId in dict) {
   const occurrences = dict[userId];
 
