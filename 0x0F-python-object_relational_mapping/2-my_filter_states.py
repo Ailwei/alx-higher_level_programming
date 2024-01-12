bc972cbf060b1870f936e4fb3fa307b4cd2a594f
Ailwei <Ailweimaemu@gmail.com>
2024-01-12 02:57:34 -0800

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,44 @@
 #!/usr/bin/python3
-"""i
-Write a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+Write a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument.
 """
 
 import MySQLdb
 from sys import argv
 
-"""
-Access to the database and get the states
-from the database.
-"""
-
 if __name__ == '__main__':
-    con = MySQLdb.connect(
-            host="localhost", port=3306, user=argv[1], password=argv[2], db=argv[3])
-    cur = con.cursor()
-    state_name = argv[4]
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (state_name,))
-    row_selected = cur.fetchall()
-    for row in row_selected:
-        print(row)
+    try:
+        """
+        Access to the database and get states from the database.
+        """
+
+        # Establish a connection to the MySQL server
+        con = MySQLdb.connect(
+            host="localhost", user=argv[1], port=3306,
+            passwd=argv[2], db=argv[3])
+
+        # Create a cursor object to interact with the database
+        cur = con.cursor()
+
+        # Create the SQL query using user input
+        query = "
+        SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4])
+
+        # Execute the query
+        cur.execute(query)
+
+        # Fetch all the rows selected by the query
+        rows_selected = cur.fetchall()
+
+        # Display the results
+        for row in rows_selected:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+
+    finally:
+        # Close the database connection
+        if con:
+            con.close()
