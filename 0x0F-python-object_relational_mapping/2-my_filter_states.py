74ac8b0f5d60eb05a1b8c84f30ba5c9b2873e071
Ailwei <Ailweimaemu@gmail.com>
2024-01-12 03:18:15 -0800

    3. SQL Injection...

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,44 +1,28 @@
 #!/usr/bin/python3
 """
-Write a script that takes in an argument and displays all values in the
-states table of hbtn_0e_0_usa where name matches the argument.
+This script takes in an argument and displays all values in the states
+where `name` matches the argument from the database `hbtn_0e_0_usa`.
+This script is designed to be safe from MySQL injections.
 """
 
-import MySQLdb
+import MySQLdb as db
 from sys import argv
 
-if __name__ == '__main__':
-    try:
-        """
-        Access to the database and get states from the database.
-        """
+if __name__ == "__main__":
+    """
+    Access the database and retrieve the states
+    based on the provided argument.
+    """
+    db_connect = db.connect(
+        host="localhost", port=3306,
+        user=argv[1], passwd=argv[2], db=argv[3])
 
-        # Establish a connection to the MySQL server
-        con = MySQLdb.connect(
-            host="localhost", user=argv[1], port=3306,
-            passwd=argv[2], db=argv[3])
+    db_cursor = db_connect.cursor()
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
 
-        # Create a cursor object to interact with the database
-        cur = con.cursor()
+    rows_selected = db_cursor.fetchall()
 
-        # Create the SQL query using user input
-        query = "
-        SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(argv[4])
-
-        # Execute the query
-        cur.execute(query)
-
-        # Fetch all the rows selected by the query
-        rows_selected = cur.fetchall()
-
-        # Display the results
-        for row in rows_selected:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQL Error: {}".format(e))
-
-    finally:
-        # Close the database connection
-        if con:
-            con.close()
+    for row in rows_selected:
+        print(row)
