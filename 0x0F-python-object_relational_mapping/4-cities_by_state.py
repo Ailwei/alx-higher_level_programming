b9bb4273dced6b7391e730be2deac2b5004bce2f
Ailwei <Ailweimaemu@gmail.com>
2024-01-12 04:11:03 -0800

    4. Cities by states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,42 +1,35 @@
 #!/usr/bin/python3
-
 """
-Write a script that lists all cities from the database hbtn_0e_4_usa
+This script lists all cities from the database `hbtn_0e_4_usa`.
+
+Usage:
+    - Ensure that you have MySQLdb installed: pip install mysqlclient
+    - Provide the necessary arguments (username, password, database name) when executing the script.
+
+Example:
+    ./script_name.py <username> <password> <database>
+
+The script connects to the specified MySQL database running on localhost at port 3306.
+It retrieves and displays information about cities from the 'hbtn_0e_4_usa' database.
 """
 
-import MySQLdb
+import MySQLdb as db
 from sys import argv
 
 if __name__ == '__main__':
     """
-    Access the database and get cities from the database.
+    Access the database and get the cities from the database.
     """
-    try:
-        # Check if the correct number of arguments is provided
-        if len(argv) != 4:
-            print("Usage: {} <username> <password> <database>".format(argv[0]))
-            exit(1)
 
-        # Get MySQL credentials from command line arguments
-        username, password, database = argv[1], argv[2], argv[3]
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
 
-        # Connect to MySQL server
-        con = MySQLdb.connect(
-            host="localhost", user=username, port=3306, passwd=password, db=database)
+    with db_connect.cursor() as db_cursor:
+        db_cursor.execute("SELECT cities.id, cities.name, states.name \
+                                FROM cities JOIN states ON cities.state_id \
+                                = states.id ORDER BY cities.id ASC")
+        rows_selected = db_cursor.fetchall()
 
-        cur = con.cursor()
-
-        # Use execute() to fetch all cities in ascending order by cities.id
-        cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
-        rows_selected = cur.fetchall()
-
+    if rows_selected is not None:
         for row in rows_selected:
             print(row)
-
-    except MySQLdb.Error as e:
-        print("Error connecting to the database:", e)
-
-    finally:
-        if 'con' in locals() or 'con' in globals():
-            cur.close()
-            con.close()
