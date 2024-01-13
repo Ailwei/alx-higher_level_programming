b2d53b04fd2aa0d9f9ea6fe1d47e69d75d1e41c7
Ailwei <Ailweimaemu@gmail.com>
2024-01-13 08:42:26 -0800

    16. List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
 """
-Lists all State objects and their corresponding
-City objects contained in the database.
-This script takes three arguments: MySQL username, MySQL password, and databas.
-The connection to the MySQL server is established on localhost at port 3306.
-Results are sorted in ascending order by states.id and cities.id.
-The output format is as follows:
-    <state id>: <state name>
-        <city id>: <city name>
+Script that lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa.
 """
 
-import sys
-from relationship_state import Base, State
-from relationship_city import City
+from sys import argv
+from relationship_city import City, Base
+from relationship_state import State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    """
+    Connects to the database and lists State and City objects.
+    """
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)I
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    states = session.query(State).outerjoin(City).order_by(
-            State.id,
-            City.id).all()
+    states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
-            print("    {}: {}".format(city.id, city.name))
+            print("\t{}: {}".format(city.id, city.name))
+
+    session.close()
