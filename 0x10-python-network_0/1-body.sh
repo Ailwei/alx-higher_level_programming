910a74ee59844a784645e5ed2a1a8f5c42dbfef1
Ailwei <Ailweimaemu@gmail.com>
2024-01-25 03:20:11 -0800

    1. cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <URL>"
     exit 1
 fi
 
 # Get the URL from the command line argument
+
 url=$1
 
 # Use curl to send a GET request and display the body for a 200 status code
+
 if curl -sI "$url" | grep -q "HTTP/1.1 200 OK"; then
     curl -s "$url"
 fi
