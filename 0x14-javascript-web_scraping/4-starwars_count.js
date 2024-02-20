c498028583c3b48260dd815ded60e6939feec7d0
Ailwei <Ailweimaemu@gmail.com>
2024-02-20 05:24:50 -0800

    4. Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl = process.argv[2];
+const charID = '18';
+let movieCount = 0;
+
+request.get(apiUrl, (error, response, body) => {
+	if (error) {
+		console.log(error);
+	} else {
+		const data = Json.parse(body);
+		data.results.forEach((film) => {
+			film.characters.forEach((character) => {
+				if (character.includes(charID)) {
+					movieCount++;
+				}
+			});
+		});
+		console.log(movieCount);
+	}
+});
